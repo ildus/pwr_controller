4e4adbcaae995ec1b266cd2a9058c1e4e20ce520
Ildus K <i.kurbangaliev@gmail.com>
2017-06-10 20:43:37 +0300

    fix formating

--- spi.c
+++ spi.c
@@ -15,30 +15,30 @@
 void spi_init(int spi_mode)
 {
 	USICR &= ~(_BV(USISIE) | _BV(USIOIE) | _BV(USIWM1));
-    USICR |= _BV(USIWM0) | _BV(USICS1) | _BV(USICLK);
-    PORT_SPI |= _BV(DD_SCK);	  //set the USCK pin as output
-    PORT_SPI |= _BV(DD_MISO);     //set the MISO pin as output
-    PORT_SPI &= ~_BV(DD_MOSI);    //set the MOSI pin as input
-    DDR_SPI &= ~((1<<DD_MOSI)|(1<<DD_MISO)|(1<<DD_SS)|(1<<DD_SCK));
+	USICR |= _BV(USIWM0) | _BV(USICS1) | _BV(USICLK);
+	PORT_SPI |= _BV(DD_SCK);	  //set the USCK pin as output
+	PORT_SPI |= _BV(DD_MISO);     //set the MISO pin as output
+	PORT_SPI &= ~_BV(DD_MOSI);    //set the MOSI pin as input
+	DDR_SPI &= ~((1<<DD_MOSI)|(1<<DD_MISO)|(1<<DD_SS)|(1<<DD_SCK));
 
 	if (spi_mode == SPI_MODE1)
-        USICR |= _BV(USICS0);
-    else
-        USICR &= ~_BV(USICS0);
+		USICR |= _BV(USICS0);
+	else
+		USICR &= ~_BV(USICS0);
 }
 
 /* Shift byte through target device and get one byte */
 uint8_t spi_transfer_byte(uint8_t data)
 {
 	USIDR = data;
-    USISR = _BV(USIOIF);                //clear counter and counter overflow interrupt flag
-    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) { //ensure a consistent clock period
-        while ( !(USISR & _BV(USIOIF)) ) USICR |= _BV(USITC);
-    }
-    return USIDR;
+	USISR = _BV(USIOIF);                //clear counter and counter overflow interrupt flag
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) { //ensure a consistent clock period
+		while ( !(USISR & _BV(USIOIF)) ) USICR |= _BV(USITC);
+	}
+	return USIDR;
 }
 
 void spi_end(void)
 {
-    USICR &= ~(_BV(USIWM1) | _BV(USIWM0));
+	USICR &= ~(_BV(USIWM1) | _BV(USIWM0));
 }
